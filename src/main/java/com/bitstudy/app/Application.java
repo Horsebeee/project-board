0ab046a1d54ec10f08ddf28a1953527bcdc25956
Horsebeee <jyjun83@naver.com>
2022-12-19 16:17:06 +0900

    #6 - 스프링부트 프로젝트 시작하기
    들어간 기술
    * 스프링 엑츄에이터
    * 스프링 데브툴즈
    * 롬복
    * 스프링 웹

---
+++ src/main/java/com/bitstudy/app/Application.java
@@ -0,0 +1,13 @@
+package com.bitstudy.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
