718e368408b701ce6c55185f49ac968a2aaa00a6
Horsebeee <jyjun83@naver.com>
2022-12-20 22:56:57 +0900

    게시판api설계 수정 및 정리 파일 추가
    게시판api설계.xlsx 파일 수정
    annotationArr.txt 파일 추가
     - 어노테이션 정리
    ArrNote.txt 파일 추가
     - 수업내용 정리

--- src/main/java/com/bitstudy/app/config/JpaConfig.java
+++ src/main/java/com/bitstudy/app/config/JpaConfig.java
@@ -1,6 +1,5 @@
 package com.bitstudy.app.config;
 
-import com.sun.xml.bind.v2.TODO;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
