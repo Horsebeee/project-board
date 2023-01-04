e159f55fd57e00789d2f1f080849630ed2c27b9d
Horsebeee <jyjun83@naver.com>
2023-01-04 17:52:44 +0900

    #17 - 게시판 view 기본 골격 작성 1
    * view 대략적인 structure 작성
              - templates > index.html 파일 생성
    * 컨트롤러에 테스트를 통과할 수 있도록 맵핑, 모델 세팅

--- src/main/java/com/bitstudy/app/controller/ArticleController.java
+++ src/main/java/com/bitstudy/app/controller/ArticleController.java
@@ -1,6 +1,12 @@
 package com.bitstudy.app.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
 
 /* 할 일 : view 의 EndPoint 관련 컨트롤러
 
@@ -14,7 +20,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
                 그 외의 css ,img ,js 들은 resources > static 폴더 안에 작성 가능
 
 */
+@Controller
 @RequestMapping("/articles") // 모든 경로들은 / articles 로 시작하니까 클래스 레벨에 1차로 @RequestMapping("/articles") 걸기
 public class ArticleController {
 
+    @GetMapping
+    public String atricles(ModelMap map) {
+        /**
+            ModelMap : 테스트파일에서 .andExpect(model().attributeExists("articles/
+
+
+        */
+        map.addAttribute("articles",List.of());
+        return "index";
+    }
 }
